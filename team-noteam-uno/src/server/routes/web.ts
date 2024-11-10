c9edae4d21947e715115af9c41aa32ebc17ba854
Aldo Zaboni <aldo.zaboni96@gmail.com>
2024-11-10 08:01:13 -0800

    created main menu

--- team-noteam-uno/src/server/routes/web.ts
+++ team-noteam-uno/src/server/routes/web.ts
@@ -1,11 +1,24 @@
 import express from "express";
 const router = express.Router();
 
-// Home page route
+// Home/Main Menu route
 router.get("/", (_req, res) => {
-    res.render("home", {
-        title: "Welcome",
-        message: "Welcome to the application"
+    res.render("landing", {
+        title: "UNO Game - Main Menu",
+        username: "Player" // This will be replaced with actual user data later
+    });
+});
+
+// Game Routes
+router.get("/play", (_req, res) => {
+    res.render("gameLobby", {
+        title: "Game Lobby"
+    });
+});
+
+router.get("/leaderboard", (_req, res) => {
+    res.render("leaderboard", {
+        title: "Leaderboard"
     });
 });
 
